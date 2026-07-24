e3c0b8c26ba1081a0e9dd63250f86d2babe04175
giannicappiello <giovanni.cappiello@progre.it>
2026-07-24 10:45:22 +0200

    feat: route gestione agenti Mexal

--- src/modules/integrations/IntegrationsModule.jsx
+++ src/modules/integrations/IntegrationsModule.jsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthContext";
 import IntegrationsDashboard from "./pages/IntegrationsDashboard";
 import MexalDashboard from "./pages/MexalDashboard";
+import MexalAgents from "./pages/MexalAgents";
 import DocumentSeriesSettings from "./pages/DocumentSeriesSettings";
 import OrderModuleSettings from "./components/OrderModuleSettings";
 import "./integrations.css";
@@ -22,6 +23,7 @@ export default function IntegrationsModule() {
     <Routes>
       <Route index element={<IntegrationsDashboard />} />
       <Route path="mexal" element={<MexalDashboard />} />
+      <Route path="mexal/agenti" element={<MexalAgents />} />
       <Route path="mexal/serie-documenti" element={<DocumentSeriesSettings />} />
       <Route path="orders/prof" element={<OrderModuleSettings moduleCode="prof" />} />
       <Route path="orders/ph" element={<OrderModuleSettings moduleCode="ph" />} />
